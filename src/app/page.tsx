ae1b8c01a39e831b668d3f8c7ab8466fb7c34414
beetheprogrammer <106383432+beetheprogrammer@users.noreply.github.com>
2025-08-30 09:47:41 -0400

    Work History Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Projects from "./projects/page";
+import WorkHistoryPage from "./work-history/page";
 
 export default function Home() {
   return (
     <div >
       <Projects/>
+      <WorkHistoryPage/>
     </div>
   );
 }
